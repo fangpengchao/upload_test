6bef2cc8d9e6cf212b0a76ba9ea186fe3f157c1f
836627727 <836627727@qq.com>
2021-07-06 15:01:25 +0800

    123

--- 源1.cpp
+++ 源1.cpp
@@ -1,7 +1,9 @@
 #include<iostream>
 int main() {
 	std::cout << "hello world!" << std::endl;
-
+	std::cout << 1+1 << std::endl;
+	std::cout << 1 + 1 << std::endl;
+	std::cout << 1 + 1 << std::endl;
 	system("pause");
 	return 0;
 }
