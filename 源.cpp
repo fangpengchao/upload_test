5853b88eac2cc4852aacb96464413d90137cf0f5
836627727 <836627727@qq.com>
2021-07-06 15:15:19 +0800

    123

--- 源.cpp
+++ 源.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 int main() {
-	std::cout << "hello world!" << std::endl;
+	std::cout << "hello1 world!" << std::endl;
 	std::cout << 1+1 << std::endl;
 	std::cout << 1 + 2 << std::endl;
         std::cout << 1 + 2 << std::endl;
